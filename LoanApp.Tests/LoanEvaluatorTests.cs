0811195d8d5a1072b6729b570daefe6d002bac3f
batoul <batouly853.com>
2025-06-20 23:49:45 +0300

    LoanAppUpdate2

--- LoanApp.Tests/LoanEvaluatorTests.cs
+++ LoanApp.Tests/LoanEvaluatorTests.cs
@@ -1,10 +1,15 @@
-﻿namespace RiskLevelApp.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-
-    }
-}
+﻿ using Xunit;
+ using LoanApp;
+ namespace LoanApp.Tests
+ {
+ public class LoanEvaluatorTests
+ {
+ [Fact]
+ public void GetLoanEligibility_Should_Return_NotEligible_When_Income_Low()
+ {
+ var evaluator = new LoanEvaluator();
+ var result = evaluator.GetLoanEligibility(1500, true, 800, 5, true);
+ Assert.Equal("Not Eligible", result);
+ }
+ }
+ }
